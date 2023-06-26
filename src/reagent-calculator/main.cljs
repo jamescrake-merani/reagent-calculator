e5e68352efeab05650c903234706346fa65ae61e
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-06-26 14:29:41 +0100

    Wrote initial main cljs file.

---
+++ src/reagent-calculator/main.cljs
@@ -0,0 +1,12 @@
+(ns reagent-calculator.main
+  [:require [reagent.core :as r]
+   [reagent.dom :as rd]])
+
+(defn base []
+ [:div
+  [:h1 "Placeholder Header"]])
+
+(defn render
+  []
+  (rd/render [base]
+             (-.getElementById js/document "app")))
