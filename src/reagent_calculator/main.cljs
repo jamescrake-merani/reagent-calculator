f8f209f9ffebf0bb083955b6031e3040ba950ab7
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-06-26 17:39:47 +0100

    Created a component for the calculator.

--- src/reagent_calculator/main.cljs
+++ src/reagent_calculator/main.cljs
@@ -2,9 +2,16 @@
   [:require [reagent.core :as r]
    [reagent.dom :as rd]])
 
+(defn calculator
+  []
+  [:div
+   [:input {:type "text"}]])
+
 (defn base []
  [:div
-  [:h1 "Placeholder Header"]])
+  [:h1 "Reagent Calculator"]
+  [calculator]])
+
 
 (defn render
   []
