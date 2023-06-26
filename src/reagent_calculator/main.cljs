2131750e7d6a1a2dd7bfff5afd88bd086fe06adf
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-06-26 18:01:12 +0100

    Programically create the number buttons.

--- src/reagent_calculator/main.cljs
+++ src/reagent_calculator/main.cljs
@@ -2,10 +2,21 @@
   [:require [reagent.core :as r]
    [reagent.dom :as rd]])
 
+(defn build-buttons
+  []
+  (map
+   (fn [value]
+     ;; TODO: Printing is a placeholder
+     [:button {:on-click #(js/console.log value)} value])
+   [1 2 3 4 5 6 7 8 9]))
+
 (defn calculator
   []
   [:div
-   [:input {:type "text"}]])
+   [:input {:type "text"}]
+   [:div {:id "btn-columns"}
+    [:div {:id "calc-buttons"}
+     (build-buttons)]]])
 
 (defn base []
  [:div
